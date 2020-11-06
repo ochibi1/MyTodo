69e75ebfe326cca25c9a367200879f837b0fa45d
ochibi1 <lapotit0122@gmail.com>
2020-11-06 11:21:42 +0900

    tmp commit before push to confirm the difference btwn feature/202010/main_screen_view & feature/sample/matsumoto

--- MyTodo/ContentViewController.swift
+++ MyTodo/ContentViewController.swift
@@ -10,43 +10,45 @@ import UIKit
 // プロトコルを２つ追加している
 class ContentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
-        // テーブルに表示させるデータを用意（DBが設計時に漏れていたため、一時的な見た目の確認用）
-        var items = ["aa", "bb", "cc", "ee"]
+    // テーブルに表示させるデータを用意（DBが設計時に漏れていたため、一時的な見た目の確認用）
+    var items = ["aa", "bb", "cc", "ee"]
 
-        required init?(coder: NSCoder){
-            super.init(coder: <#T##NSCoder#>)
-            
-            super.viewDidLoad()
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
 
-            // テーブルを用意して、表示
-            let table: UITableView = UITableView(frame: CGRect(x: 50, y: 50 , width: 200, height: 500))
-            table.register(UITableViewCell.self, forCellReuseIdentifier: "data")
-            table.dataSource = self
-            table.delegate = self
-            self.view.addSubview(table)
-    
-        }
-    
+    init(){
+        super.init(nibName: nil, bundle: nil)
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-            cell.textLabel?.text = items[indexPath.row]
-            return cell
-        }
-        /*
-        下の２つのfuncはDataSourceのプロトコルに定められているモノなので、記載しないとエラーになる
-        */
+        // テーブルを用意して、表示
+        let table: UITableView = UITableView(frame: CGRect(x: 50, y: 50 , width: 200, height: 500))
+        table.register(TableViewCell.self, forCellReuseIdentifier: "data")
+        table.dataSource = self
+        table.delegate = self
+        self.view.addSubview(table)
 
-        // セルの行数を指定
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return items.count
-        }
+    }
 
-        // セルの値を設定
-        private func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "data", for: indexPath as IndexPath) as UITableViewCell
-            cell.textLabel?.text = items[indexPath.row]
-            return cell
-        }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "data", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    /*
+    下の２つのfuncはDataSourceのプロトコルに定められているモノなので、記載しないとエラーになる
+    */
+
+    // セルの行数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    // セルの値を設定
+    private func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "data", for: indexPath as IndexPath) as UITableViewCell
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 
 }
---
+++ MyTodo/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  MyTodo
+//
+//  Created by Laflora on 2020/11/05.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
