04bedce3cf08d4d764b10e70875b91155cdcec65
ochibi1 <lapotit0122@gmail.com>
2020-11-11 11:18:07 +0900

    Model class files default ver.

---
+++ Task+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Task+CoreDataProperties.swift
+//  
+//
+//  Created by Laflora on 2020/11/11.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var list: String?
+
+}
