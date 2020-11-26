7f99120d198a9821e4ef1c46c22ff467d55e3534
ochibi1 <lapotit0122@gmail.com>
2020-11-26 14:04:30 +0900

    add an attribute(taskNumber) and fix autolayout in add_view

--- MyTodo/ItemAddViewController.swift
+++ MyTodo/ItemAddViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 
 class ItemAddViewController: UIViewController {
     let inputField: UITextField! = UITextField(frame: .zero)
+    let numberField: UITextField! = UITextField(frame: .zero)
     let saveBtn = UIButton(frame: .zero)
 
     init() {
@@ -17,15 +18,26 @@ class ItemAddViewController: UIViewController {
         self.title = "新規項目追加画面"
         self.view.backgroundColor = .lightGray
         
-        self.inputField.borderStyle = .roundedRect
-        self.view.addSubview(self.inputField)
-        self.inputField.snp.makeConstraints { make in
+        self.numberField.borderStyle = .roundedRect
+        self.view.addSubview(self.numberField)
+        self.numberField.snp.makeConstraints { make in
             make.top.equalTo(150.0)
             make.height.equalTo(45.0)
             make.left.equalTo(40.0)
             make.right.equalTo(-40.0)
         }
+        self.numberField.placeholder = "優先順位の番号を入力"
+        
+        self.inputField.borderStyle = .roundedRect
+        self.view.addSubview(self.inputField)
+        self.inputField.snp.makeConstraints { make in
+            make.top.equalTo(self.numberField.snp.bottom).offset(30.0)
+            make.height.equalTo(self.numberField.snp.height)
+            make.left.equalTo(self.numberField.snp.left)
+            make.right.equalTo(self.numberField.snp.right)
+        }
         self.inputField.placeholder = "今日のやることを入力"
+        
         
         self.saveBtn.setTitle("保存", for: .normal)
         self.saveBtn.backgroundColor = .green
@@ -50,13 +62,21 @@ class ItemAddViewController: UIViewController {
         //inputFieldに入力されていないのに、saveDataが呼び出された場合、この処理を返して終了にするためのguard節。
         guard let taskTitle = self.inputField.text,
               !taskTitle.isEmpty else {
-            let alert = UIAlertController(title: "エラー", message: "入力してから保存ボタンを押してください", preferredStyle: .alert)
+            let alert = UIAlertController(title: "エラー", message: "今日のやることを入力してから保存ボタンを押してください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        guard let taskPriority = self.numberField.text,
+              !taskPriority.isEmpty else {
+            let alert = UIAlertController(title: "エラー", message: "優先順位の数字を入力してから保存ボタンを押してください", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             return
         }
         let data = Task.create()
         data.taskName = taskTitle
+        data.taskNumber = taskPriority
         data.insert()
         let isSuccess = data.save()
         if isSuccess {
