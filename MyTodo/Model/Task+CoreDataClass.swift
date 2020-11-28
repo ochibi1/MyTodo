1708a7156de5584957896b4dc197170d301441fd
ochibi1 <lapotit0122@gmail.com>
2020-11-28 14:30:52 +0900

    validation practice

--- MyTodo/Model/Task+CoreDataClass.swift
+++ MyTodo/Model/Task+CoreDataClass.swift
@@ -12,4 +12,17 @@ import UIKit
 
 public class Task: NSManagedObject, BaseModelProtocol {
     
+    func isNumberValid(_ taskNumber: String) -> Bool {
+        
+        return !taskNumber.isEmpty
+            && Int16(taskNumber) != nil
+            && Int16(taskNumber)! > 0
+            && taskNumber == "^[０-９]+"
+            && taskNumber == "^[A-Z]+"
+        
+        //顔文字のような記号が含まれていないか
+        //16進数の文字列ではないか（これについては、どうやって正規表現とか使うのかイメージ湧いていない）
+
+    }
+    
 }
