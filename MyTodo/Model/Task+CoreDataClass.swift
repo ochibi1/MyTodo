04cb88d1361523cd5eb099fe7526385e2483cf37
ochibi1 <lapotit0122@gmail.com>
2020-11-30 12:18:08 +0900

    add proper validate refering to the sample

--- MyTodo/Model/Task+CoreDataClass.swift
+++ MyTodo/Model/Task+CoreDataClass.swift
@@ -12,28 +12,12 @@ import UIKit
 
 public class Task: NSManagedObject, BaseModelProtocol {
     
-    func isNumberValid(_ taskNumber: String) -> Bool {
-        
-        return taskNumber.isEmpty
-            && Int16(taskNumber) != nil
-            && Int16(taskNumber)! > 0
-            && taskNumber == "^[０-９]+"
-            && taskNumber == "^[A-Z]+"
-            && taskNumber == "^(0x)?([0-9a-f]{2,})"
-            && taskNumber.isAppleColorEmoji
-    }
-    
-}
-
-fileprivate var appleColorEmojiFont = CTFontCreateWithName("AppleColorEmoji" as CFString, 20, nil)
-
-extension String {
-    var isAppleColorEmoji: Bool {
-        let chars = Array(self.utf16)
-        if chars.count == 1 && chars[0] <= 57 { 
+    func isNumberValid(taskNumber: String?) -> Bool {
+        guard let taskNumber = taskNumber,
+              let regex = try? NSRegularExpression(pattern: "[^0-9]"),
+              regex.numberOfMatches(in: taskNumber, options: [], range: NSRange(location: 0, length: taskNumber.count)) <= 0 else {
             return false
         }
-        var glyphs = [CGGlyph](repeating: 0, count: chars.count)
-        return CTFontGetGlyphsForCharacters(appleColorEmojiFont, chars, &glyphs, glyphs.count)
+        return true
     }
 }
